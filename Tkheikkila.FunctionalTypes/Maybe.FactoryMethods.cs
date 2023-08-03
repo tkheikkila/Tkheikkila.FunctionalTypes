7ca9dcb330a4dfa349c33ca785dbed8795c1d240
Toni Heikkilä <tkheikkila@outlook.com>
2023-08-03 09:07:43 +0300

    Add Maybe factory methods

--- Tkheikkila.FunctionalTypes/Maybe.FactoryMethods.cs
+++ Tkheikkila.FunctionalTypes/Maybe.FactoryMethods.cs
@@ -2,6 +2,11 @@
 
 public static class Maybe
 {
+    public static Unit None()
+    {
+        return Unit.Value;
+    }
+
     public static Maybe<T> None<T>()
     {
         return new Maybe<T>();
@@ -10,6 +15,11 @@ public static class Maybe
     public static Maybe<T> None<T, TDiscard>(TDiscard _)
     {
         return None<T>();
+    }
+
+    public static Maybe<Unit> Some()
+    {
+        return new Maybe<Unit>(Unit.Value);
     }
 
     public static Maybe<T> Some<T>(T value)
@@ -25,5 +35,15 @@ public static class Maybe
     public static Task<Maybe<T>> SomeTask<T>(T value)
     {
         return Task.FromResult(Some(value));
+    }
+
+    public static ValueTask<Maybe<T>> NoneValueTask<T>()
+    {
+        return new ValueTask<Maybe<T>>(None<T>());
+    }
+
+    public static ValueTask<Maybe<T>> SomeValueTask<T>(T value)
+    {
+        return new ValueTask<Maybe<T>>(Some(value));
     }
 }
