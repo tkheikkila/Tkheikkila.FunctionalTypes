fa903c9d710e34159d490ea54cea5787c092badb
Toni Heikkilä <tkheikkila@outlook.com>
2024-10-30 12:27:36 +0200

    Add SomeOrNone factory methods

--- Tkheikkila.FunctionalTypes/Maybe.FactoryMethods.cs
+++ Tkheikkila.FunctionalTypes/Maybe.FactoryMethods.cs
@@ -6,7 +6,9 @@ namespace Tkheikkila.FunctionalTypes;
 public readonly partial struct Maybe<T>
 {
 	public static Maybe<T> Some(T value) => new(value);
-    public static Maybe<T> SomeNullAsNone(T? value) => value is not null ? Some(value) : None();
+    public static Maybe<T> SomeNullAsNone(T? value) => SomeOrNone(value!, when: value is not null);
+	public static Maybe<T> SomeOrNone(T value, bool when) => when ? Some(value) : None();
+	public static Maybe<T> SomeOrNone(T value, Func<T, bool> when) => SomeOrNone(value, when(value));
 	public static Maybe<T> None() => new();
     public static Maybe<T> None<TDiscard>(TDiscard _) => new();
 }
